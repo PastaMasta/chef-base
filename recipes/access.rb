f634dca5e13a622b980acabe14478502228ee50a
PastaMasta <pasta.masta2902@gmail.com>
2015-09-13 17:12:13 +0100

    Added common ssh key

---
+++ recipes/access.rb
@@ -0,0 +1,23 @@
+#
+# Author:: PastaMasta (<pasta.masta2902@gmail.com>)
+# Cookbook Name:: basic
+# Recipe:: access
+#
+# Copyright 2015, PastaMasta
+#
+
+# Add my generic SSH access key.
+
+directory '/root/.ssh' do
+  owner 'root'
+  group 'root'
+  mode 0700
+  action :create
+end
+
+cookbook_file '/root/.ssh/authorized_keys' do
+  source 'authorized_keys'
+  owner 'root'
+  group 'root'
+  mode 0700
+end
