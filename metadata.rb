5befca273b77bc7d1a10a5ea03b05a38a4e8c313
PastaMasta <pasta.masta2902@gmail.com>
2016-12-20 22:00:06 +0000

    Fixed metadata supports

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Baseline standard cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.0'
 
-supports 'centos','~>6'
-supports 'centos','~>7'
+supports 'centos','~>6.0'
+supports 'centos','~>7.0'
 
 depends 'line'
