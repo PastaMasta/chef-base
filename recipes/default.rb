93abe59a61f9f5cad9797e313a2bc78c7904cc0e
PastaMasta <pasta.masta2902@gmail.com>
2017-06-20 21:42:04 +0100

    Updated local repo setup

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,7 +12,7 @@ masters = search(:node, 'recipes:"chef-master"') rescue nil
 node.default['master'] = masters.first.name unless masters.nil? || masters.empty?
 
 recipes = [
-  #cookbook_name + '::repos',
+  cookbook_name + '::repos',
   cookbook_name + '::data-repo'#,
   #cookbook_name + '::users',
   #cookbook_name + '::misc'
--- recipes/repos.rb
+++ recipes/repos.rb
@@ -3,23 +3,25 @@
 # Cookbook Name:: base
 # Recipe:: repos
 #
-# Copyright 2015, PastaMasta
+# Copyright 2017, PastaMasta
 #
 
 # Setup repos
 case "#{node['platform']} #{node['platform_version']}"
 when /centos [6-7]/
 
-  # Replace default repos with high cost ones
-  template '/etc/yum.repos.d/CentOS-Base.repo' do
-    source "repos/CentOS-Base.repo.erb"
-    action :create
+  # Remove system defaults
+  Dir.entries('/etc/yum.repos.d').select {|a|a.match(/^CentOS.*\.repo/)}.each do |repo|
+    file '/etc/yum.repos.d/' + repo do
+      action :delete
+    end
   end
 
   # Setup local mirrors
-  template '/etc/yum.repos.d/Local-CentOS-Base.repo' do
-   source 'repos/Local-CentOS-Base.repo.erb'
+  template '/etc/yum.repos.d/Local-CentOS.repo' do
+    source 'repos/Local-CentOS.repo.erb'
     action :create
-  end unless node['base']['master'] == 'masterless'
+    variables({ master: node['master'] })
+  end if node['master']
 
 end
