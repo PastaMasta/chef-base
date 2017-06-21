1cb35acae4251dca398c6af31488929d8bd47f8e
PastaMasta <pasta.masta2902@gmail.com>
2017-06-21 21:01:54 +0100

    Clocksync for CentOS

--- recipes/misc.rb
+++ recipes/misc.rb
@@ -9,7 +9,13 @@
 # Make sure we have some clocksync
 case "#{node['platform']} #{node['platform_version']}"
 when /centos 6/
+  package 'ntpd'
   service 'ntpd' do
+    action ['enable','start']
+  end
+when /centos 7/
+  package 'chrony'
+  service 'chronyd' do
     action ['enable','start']
   end
 end
