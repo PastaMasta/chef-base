7b7b0259d692b2cd07b2b95dadfe8b5764495888
PastaMasta <pasta.masta2902@gmail.com>
2018-04-04 22:02:16 +0100

    default update

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,6 @@
 #
 # Copyright 2015, PastaMasta
 #
-#
 
 # Find out who our master server is!
 masters = search(:node, 'recipes:"chef-master"') rescue nil
@@ -14,8 +13,8 @@ node.default['master'] = masters.first.name unless masters.nil? || masters.empty
 recipes = [
   cookbook_name + '::repos',
   cookbook_name + '::data-tree',
-  cookbook_name + '::misc',
-  cookbook_name + '::firewall'
+  cookbook_name + '::users',
+  cookbook_name + '::misc'
 ]
 
 include_recipe(recipes)
